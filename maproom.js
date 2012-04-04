721d528c8fd4887a0471e3804d4225d43c589789
Benno Blumenthal <benno@iri.columbia.edu>
2012-04-04 16:05:35 +0000

    fix

--- maproom.js
+++ maproom.js
@@ -275,23 +275,6 @@ location.href = "http://www.tumblr.com/share/link?url=" + encodeURIComponent(tum
 }
 }
 
-      function getElementsByAttribute(oElm, strTagName, strAttributeName, strAttributeValue){
-      var arrElements = (strTagName == "*" && oElm.all)? oElm.all : oElm.getElementsByTagName(strTagName);
-      var arrReturnElements = new Array();
-      var oAttributeValue = (typeof strAttributeValue != "undefined")? new RegExp("(^|\\s)" + strAttributeValue + "(\\s|$)", "i") : null;
-      var oCurrent;
-      var oAttribute;
-      for(var i=0; i<arrElements.length; i++){
-      oCurrent = arrElements[i];
-      oAttribute = oCurrent.getAttribute && oCurrent.getAttribute(strAttributeName);
-      if(typeof oAttribute == "string" && oAttribute.length > 0){
-      if(typeof strAttributeValue == "undefined" || (oAttributeValue && oAttributeValue.test(oAttribute))){
-      arrReturnElements.push(oCurrent);
-      }
-      }
-      }
-      return arrReturnElements;
-      }
 function doEvernoteClip(){
 var clipargs = {};
 clipargs.contentId = 'content';
